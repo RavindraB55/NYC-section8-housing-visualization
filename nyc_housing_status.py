a99e336efc83f128902c82536e763b649d67d094
Ravindra Bisram <ravindrabisram@gmail.com>
2023-02-02 15:28:19 -0500

    Working with zip

--- nyc_housing_status.py
+++ nyc_housing_status.py
@@ -9,7 +9,8 @@ from area import area
 
 # read the neighborhood population data into a DataFrame and load the GeoJSON data
 nycmap = json.load(open("nyc_zip.geojson"))
-df = pd.read_csv('2019_2020_FINAL.csv')
+#df = pd.read_csv('2019_2020_FINAL.csv')
+df = pd.read_csv('house_claim_counts_by_zip.csv')
 
 d = {}
 neighborhood = nycmap["features"]
@@ -25,16 +26,16 @@ densitys = np.random.rand(len(zips)) *10
 zip_df['density'] = densitys
 
 # call Plotly Express choropleth function to visualize data
-fig = px.choropleth_mapbox(zip_df,
+fig = px.choropleth_mapbox(df,
                         geojson=nycmap,
-                        locations="Zip",
+                        locations="zip",
                         featureidkey="properties.ZIPCODE",
-                        color="density",
-                        color_continuous_scale="viridis",
+                        color="Claim",
+                        color_continuous_scale="YlGnBu", #"viridis",
                         mapbox_style="carto-positron",
                         zoom=9, center={"lat": 40.7, "lon": -73.9},
                         opacity=0.7,
-                        hover_name="Names"
+                        hover_name="zip"
                         )
 
 fig.show()
